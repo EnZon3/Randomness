783333b296e22176e5fa8a6fdcae2ed922fdc250
EnZon3 <37979038+EnZon3@users.noreply.github.com>
2021-04-03 11:23:35 -0400

    s

---
+++ Some other fun stuff/DOS-install.bat
@@ -0,0 +1,11 @@
+@echo off
+echo Microsoft MS-DOS 6.22 Setup
+echo ___________________________
+echo .
+echo .
+echo          Welcome to Setup.
+echo .
+echo          The setup program prepares MS-DOS 6.22 to run on your computer.
+echo .
+echo              • To setup MS-DOS now, press 1.
+choice /c:12 /t:90 /d 2 /n /m "1=Continue    2=Exit"
